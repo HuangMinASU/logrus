427710f58a334e30b6cac93016de16ee7ef63163
HuangMin <huangmin@asu.edu>
2025-07-22 00:21:55 +0800

    OK,能拦截成功

--- main.go
+++ main.go
@@ -11,12 +11,16 @@ import (
 // 是否美化输出的全局开关
 var prettyPrint = false
 
-// FormatJSON 根据全局开关决定是否美化输出 JSON
+// FormatJSON 根据全局开关决定是否美化输出 JSON，并对特定字段进行脱敏处理
 func FormatJSON(message string) (string, error) {
 	var jsonData map[string]interface{}
 	if err := json.Unmarshal([]byte(message), &jsonData); err != nil {
 		return fmt.Sprintf("[String] %s", message), nil
 	}
+
+	// 脱敏处理
+	DesensitizeData(jsonData, "keyToModify")
+	DesensitizeData(jsonData, "event=")
 
 	if prettyPrint {
 		prettyJSON, err := json.MarshalIndent(jsonData, "", "  ")
@@ -30,6 +34,23 @@ func FormatJSON(message string) (string, error) {
 			return message, err
 		}
 		return string(compactJSON), nil
+	}
+}
+
+// DesensitizeData 对指定键对应的值进行简单的脱敏处理
+func DesensitizeData(data map[string]interface{}, key string) {
+	if value, exists := data[key].(string); exists {
+		// 检查字符串长度，确保有足够的字符可以替换为******
+		if len(value) > 6 {
+			data[key] = value[:len(value)-6] + "******"
+		}
+	}
+
+	// 递归处理嵌套结构
+	for _, v := range data {
+		if nestedMap, ok := v.(map[string]interface{}); ok {
+			DesensitizeData(nestedMap, key)
+		}
 	}
 }
 
@@ -48,7 +69,10 @@ func (hook *CustomHook) Fire(entry *logrus.Entry) error {
 		output = entry.Message // 回退到原始信息
 	}
 
-	// 打印输出
+	// 更新 entry.Message 以确保脱敏后的消息被记录
+	entry.Message = output
+
+	// 打印到控制台
 	fmt.Println(output)
 
 	return nil
@@ -76,12 +100,16 @@ func main() {
 	}
 
 	// 打印包含 JSON 数据的日志
-	jsonMessage := `{"nestedField": {"keyToModify": "OldValue"}, "anotherField": "value"}`
+	jsonMessage := `{"nestedField": {"keyToModify": "SensitiveValue"}, "anotherField": "value"}`
 	logger.Info(jsonMessage)
 
 	// 打印普通字符串的日志
 	plainMessage := "This is a plain text message."
 	logger.Info(plainMessage)
+
+	// 打印包含 JSON 数据的日志
+	jsonMessage2 := `{"nestedField": {"keyToModify": "22543322555"}, "anotherField": "value"}`
+	logger.Info(jsonMessage2)
 
 	// 使用带有字段的日志
 	logger.WithFields(logrus.Fields{
